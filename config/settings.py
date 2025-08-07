0565d304e4b7868cd849fa24f2c994334fd900af
bobdreads <mauriciofilho60@gmail.com>
2025-08-07 19:00:16 -0300

    feat: atualizar configurações do Django e melhorar a interface do admin

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Apps padrão do Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -114,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,58 +1,51 @@
 # Em dashboard/admin.py
 
 from django.contrib import admin
+# Não importamos mais nada do 'unfold'
 from .models import Asset, Strategy, Tag, Operation, Movement, Attachment
-
-# --- Customização do Admin para uma melhor experiência ---
-
-# Isto permite editar 'Movimentações' e 'Anexos' DENTRO da página de uma 'Operação'
-# É muito mais prático do que ter que criar cada um separadamente.
 
 
 class MovementInline(admin.TabularInline):
     model = Movement
-    extra = 0  # Mostra 1 campo extra para adicionar uma nova movimentação por padrão
+    extra = 0
 
 
 class AttachmentInline(admin.TabularInline):
     model = Attachment
-    extra = 0  # Mostra 1 campo extra para um novo anexo
+    extra = 0
 
 
 @admin.register(Operation)
-class OperationAdmin(admin.ModelAdmin):
-    # ... (list_display, list_filter, etc. continuam iguais) ...
+class OperationAdmin(admin.ModelAdmin):  # Garante que está usando admin.ModelAdmin
     list_display = ('id', 'user', 'asset', 'status',
                     'initial_operation_type', 'net_financial_result', 'start_date')
     list_filter = ('status', 'asset__market', 'strategy', 'user')
     search_fields = ('asset__ticker', 'user__username', 'strategy__name')
     inlines = [MovementInline, AttachmentInline]
-
-    # Organiza os campos no formulário de edição para ficarem mais legíveis
     fieldsets = (
-        ('Info Principal', {
-            'fields': ('user', 'asset', 'status', 'initial_operation_type', 'strategy')
-        }),
-
-        # --- SEÇÃO ADICIONADA AQUI ---
+        ('Info Principal', {'fields': ('user', 'asset',
+         'status', 'initial_operation_type', 'strategy')}),
         ('Plano de Trade', {
-            'fields': ('initial_stop_price', 'initial_target_price')
-        }),
-        # --- FIM DA SEÇÃO NOVA ---
-
-        ('Datas', {
-            'fields': ('start_date', 'end_date')
-        }),
-        ('Análise e Resultado', {
-            'fields': ('entry_reason', 'general_notes', 'entry_sentiment', 'execution_rating', 'tags')
-        }),
+         'fields': ('initial_stop_price', 'initial_target_price')}),
+        ('Datas', {'fields': ('start_date', 'end_date')}),
+        ('Análise e Resultado', {'fields': (
+            'entry_reason', 'general_notes', 'entry_sentiment', 'execution_rating', 'tags')}),
         ('Resultados Calculados', {
-            'fields': ('net_financial_result', 'points_pips_result')
-        }),
+         'fields': ('net_financial_result', 'points_pips_result')}),
     )
 
 
-# Registrando os outros modelos de forma simples
-admin.site.register(Asset)
-admin.site.register(Strategy)
-admin.site.register(Tag)
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    search_fields = ('ticker', 'name')
+    list_filter = ('market', 'currency')
+
+
+@admin.register(Strategy)
+class StrategyAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
