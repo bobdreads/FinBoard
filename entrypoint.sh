dcb24ba6e12520e3aabb1ac9258e2742d7ce5326
bobdreads <mauriciofilho60@gmail.com>
2025-08-21 18:14:56 -0300

    refactor: remover arquivos obsoletos e limpar o Dockerfile

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Executa as migrações do banco de dados
-echo "Applying database migrations..."
-python manage.py migrate --noinput
-
-# Inicia o processo principal (Gunicorn)
-# O "$@" executa o comando que foi passado para o script
-exec "$@"
